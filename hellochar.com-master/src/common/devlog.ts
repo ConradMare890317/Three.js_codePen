9bb8b720ff2eae5656e9a286a22bf4ebd6625494
ConradMare890317 <Conradmare@gmail.com>
2020-12-20 15:44:18 +0200

    a

--- hellochar.com-master/src/common/devlog.ts
+++
@@ -1,5 +0,0 @@
-export default function devlog(message?: any, ...optionalParams: any[]) {
-    if (process.env.NODE_ENV === "development") {
-        console.log(message, ...optionalParams);
-    }
-}
